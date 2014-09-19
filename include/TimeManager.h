e2e613751fa943e23a449081ee121c34844e58ee
winmad <winmad.wlf@gmail.com>
2014-09-19 01:02:53 -0500

    use new timer

--- include/TimeManager.h
+++ include/TimeManager.h
@@ -8,8 +8,33 @@
 
 using namespace std;
 
+/*
+ * Author:  David Robert Nadeau
+ * Site:    http://NadeauSoftware.com/
+ * License: Creative Commons Attribution 3.0 Unported License
+ *          http://creativecommons.org/licenses/by/3.0/deed.en_US
+ */
+#if defined(_WIN32)
+#include <Windows.h>
+
+#elif defined(__unix__) || defined(__unix) || defined(unix) || (defined(__APPLE__) && defined(__MACH__))
+#include <unistd.h>	/* POSIX flags */
+#include <time.h>	/* clock_gettime(), time() */
+#include <sys/time.h>	/* gethrtime(), gettimeofday() */
+
+#if defined(__MACH__) && defined(__APPLE__)
+#include <mach/mach.h>
+#include <mach/mach_time.h>
+#endif
+
+#else
+#error "Unable to define getRealTime( ) for an unknown OS."
+#endif
+
+double getRealTime();
+
 struct stopwatch_rec {
-	clock_t startTime;
+	double startTime;
 	double cumulatedTime;
 };
 
@@ -24,7 +49,7 @@ public:
 	void ResumeStopWatch(int id);
 	void DisplayStopWatch(int id, string textFormat);
 private:
-	vector<clock_t> times;
+	vector<double> times;
 	vector<stopwatch_rec> stopWatches;
 };
 
